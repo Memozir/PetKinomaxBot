c10dbd2c94e2f569adb90d012b48ff46e1e00e71
Memozir <gizmovap@gmail.com>
2021-08-23 17:36:25 +0300

    test geo_2

--- handlers/message_handler.py
+++ handlers/message_handler.py
@@ -9,12 +9,14 @@ async def start_message(message: types.Message):
 	await message.answer(messages.start['start_message'], reply_markup=keyboards.kb_start)
 
 
+@dp.message_handler(commands='geo')
+async def get_geo(message: types.Message):
+	await message.answer('Э, дай геолокацию!', reply_markup=keyboards.geo_kb)
+
+
 @dp.message_handler()
 async def display_func(message: types.Message):
 	if message.text.lower() == 'начать':
 		await message.answer('Выберите, что хотите сделать:', reply_markup=keyboards.kb_func)
 
 
-@dp.message_handler(commands='geo')
-async def get_geo(message: types.Message):
-	await message.answer('Э, дай геолокацию!', reply_markup=keyboards.geo_kb)
